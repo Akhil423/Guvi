ea21e77cca16442b307e5dace607ef92a52d9c45
Akhil Varma <akhilvarma423@gmail.com>
2017-03-16 15:37:21 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -6,9 +6,17 @@ int main(){
 string s,s1;
 cin>>s;
 int c=s.length();
+  if(c>=1){
 while(c--){
+  if(s.at(1)=='-'){
+  break;
+  }
+    
 s1+=s.at(c);
 }
+  }
+  else
+    cout<<"give input";
   cout<<s1;
 return 0;
 }
