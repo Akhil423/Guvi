ffbd1c805b03913bed3cf824657cb017e1b0784b
Akhil Varma <akhilvarma423@gmail.com>
2017-05-27 16:06:12 +0530

    Update reverse_num.cpp

--- reverse_num.cpp
+++ reverse_num.cpp
@@ -1,1 +1,29 @@
+//code for reversing number 
+#include<iostream>
+using namesapce std;
 
+void reverse_num(int num){
+  int rem;
+      if(!num){
+
+          cout<<0;
+          return 0;
+      }
+      while(num!=0){
+
+          rem=num%10;
+          cout<<rem;
+          num/=10;
+
+      }
+}
+
+int main(void){
+    
+      int num;
+      cout<<"enter the positive integer to reverse the number"<<endl;
+      cin>>num;
+      reverse_num(num);
+
+      return 0;
+}
