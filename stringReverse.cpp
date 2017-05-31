ce0a742e6583caf25ee761330be47ad43c1b4786
Akhil Varma <akhilvarma423@gmail.com>
2017-05-31 23:09:26 +0530

    Create stringReverse.cpp

---
+++ stringReverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+using namespace std;
+
+int main(){
+  string s;
+  getline(cin,s);
+  string::size_type rev;
+  int i=stoi(s,&rev)
+  cout<<i;
+  
+    return 0;
+}
