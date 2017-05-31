8d222f14ef9f85559813102f1db31e925ceb2bfd
Akhil Varma <akhilvarma423@gmail.com>
2017-06-01 00:33:28 +0530

    Create rtoi.cpp

---
+++ rtoi.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<stdlib>
+using namespace std;
+
+int fetch(char a){
+
+      if(a=='I')
+      return 1;
+      if(a=='V')
+      return 5;
+      if(a=='X')
+      return 10;
+      if(a=='L')
+      return 50;
+      if(a=='C')
+      return 100;
+      if(a=='D')
+      return 500;
+      if(a=='M')
+      return 1000;
+
+  return -1;
+}
+int rmnum(string &st){
+  
+      int integ=0,c1,c2;
+      for(int i=0;i<st.length();i++){
+        c1=fetch(st[i]);
+        if(i+1<st.length()){
+          c2=fetch(st[i+1]);
+          if(c1>=c2)
+            integ+=c1;
+          else{
+            integ+=(c2-c1);
+            i++; 
+           }
+        }
+        else
+            integ+=c1;
+      }
+
+  return integ;
+}
+
+
+int main(void){
+  
+    string s;
+    geline(cin,s);
+    int out=rmnum(s);
+    cout<<"the value of roman"<<s<<"is:"<<out;
+}
